996561d0f36f525f9bfdca1b36640650843ab000
woong <koohw0109@gmail.com>
2023-06-06 13:47:07 +0900

    #7.3 Movie App part One

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -2,55 +2,39 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setNeed(1);
-  };
-  const handleInput = (event) => {
-    setNeed(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
   return (
     <div>
-      <h1>The coins! {loading ? "" : `Here are..${coins.length} coins`}</h1>
       {loading ? (
-        <strong>loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          <option>Select Coin!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
+        </div>
       )}
-      <h2>Please enter the amount</h2>
-      <div>
-        <input
-          type="number"
-          value={need}
-          onChange={handleInput}
-          placeholder="dollor"
-        />
-        $
-      </div>
-      <h2>You can get {need / cost}</h2>
     </div>
   );
 }
