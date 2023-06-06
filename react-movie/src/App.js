295d3537dbb018abd7a560139bed1e4dfef010ae
woong <koohw0109@gmail.com>
2023-06-06 12:49:41 +0900

    #7.1 To Do List part Two

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -12,6 +12,7 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
   return (
     <div>
       <h1>My To Dos ({toDos.length})</h1>
@@ -24,6 +25,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
