bdf64dfbad1abd010aa607e94a410496b41ab73f
woong <koohw0109@gmail.com>
2023-06-06 13:25:21 +0900

    Update Coin Tracker.js

--- react-movie/src/Coin Tracker.js
+++ react-movie/src/Coin Tracker.js
@@ -3,9 +3,18 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
+  };
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((responce) => responce.json())
+      .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
@@ -13,18 +22,35 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      <h1>The coins! {loading ? "" : `Here are..${coins.length} coins`}</h1>
       {loading ? (
-        <string>Loading...</string>
+        <strong>loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+        <select onChange={onChange}>
+          <option>Select Coin!</option>
+          {coins.map((coin, index) => (
+            <option
+              key={index}
+              value={coin.quotes.USD.price}
+              id={coin.symbol}
+              symbol={coin.symbol}
+            >
+              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <h2>Please enter the amount</h2>
+      <div>
+        <input
+          type="number"
+          value={need}
+          onChange={handleInput}
+          placeholder="dollor"
+        />
+        $
+      </div>
+      <h2>You can get {need / cost}</h2>
     </div>
   );
 }
